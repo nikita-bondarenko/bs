2373b63da00725d3234667914aefc15f8d7ac3f5
Nikita Bondarenko <naradadasosmi@gmail.com>
2022-12-15 00:13:53 +0800

    first commit

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+export const nav = () => {
+    const companiesList = document.querySelector('.companies__list')
+    const companiesSection = document.querySelector('.companies')
+    const queueDelay = 100
+    const animationDuration = 300
+    let isOpening = false
+    let isClosing = false
+
+    function open() {
+        isOpening = true
+        Array.from(companiesList.children).map((item, index) => setTimeout(() => {
+            item.classList.add('open')
+            index === companiesList.children.length - 1 ? isOpening = false : 1
+            isClosing ? close() : 1
+        }, index * queueDelay))
+    }
+
+    function close()  {
+        isClosing = true
+        if (!isOpening) {
+            companiesSection.classList.add('disabled')
+            setTimeout(() => {
+                companiesSection.classList.remove('disabled')
+            }, (companiesList.children.length + 1) * queueDelay)
+
+            Array.from(companiesList.children).reverse().map((item, index) => setTimeout(() => {
+                item.classList.add('closing')
+                item.classList.remove('open')
+                setTimeout(() => {
+                    item.classList.remove('closing')
+                }, animationDuration)
+                index === companiesList.children.length - 1 ? isClosing = false : 1
+            }, index * queueDelay))
+        }
+    }
+
+    companiesSection.addEventListener('mouseenter', open)
+
+    companiesSection.addEventListener('mouseleave', close)
+
+}
