08fffdfd6ba7d6712208f715a0e1f742532dc413
Nikita Bondarenko <naradadasosmi@gmail.com>
2022-12-17 13:35:11 +0800

    isad

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@
 	$mail->IsHTML(true);
 
 	//От кого письмо
-	$mail->setFrom('naradadasosmi@gmail.com', 'BS группа компаний');
+	$mail->setFrom( 'BS группа компаний');
 	//Кому отправить
 	$mail->addAddress('brajbas3@gmail.com');
 	//Тема письма
