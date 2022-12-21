cbeb509eabb18d1fce22ca4ab5a0029a1c466849
Nikita Bondarenko <naradadasosmi@gmail.com>
2022-12-21 16:40:09 +0800

    'sdf'

--- js/forcedOriginalScale.js
+++ js/forcedOriginalScale.js
@@ -1,6 +1,6 @@
 export const forcedOriginalScale = ( containerId ) => {
 
         var App = document.getElementById( containerId ); //получаем div по его id
-        App.style.zoom = (1 / devicePixelRatio) * 1.3; //устанавливаем масштаб в зависимости от pixel-ratio
+        App.style.zoom = (1 / devicePixelRatio) * 1.5; //устанавливаем масштаб в зависимости от pixel-ratio
 
 }
